3a961ac313adf64e31c366ad92c71795c5edfd4f
Ivan Warren <ivanravebizz@gmail.com>
2022-08-10 08:22:38 -0400

    update

---
+++ app/src/main/java/com/example/linkedlist/LinkList.kt
@@ -0,0 +1,41 @@
+package com.example.linkedlist
+
+class LinkedList<T> {
+
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+
+    fun isEmpty(): Boolean {
+        return size == 0
+    }
+
+    override fun toString(): String {
+        if (isEmpty()) {
+            return "Empty list"
+        } else {
+            return head.toString()
+        }
+    }
+
+    fun push(value: T): LinkedList<T> {
+        head = Node(value = value, next = head)
+        if (tail == null) {
+            tail = head
+        }
+        size++
+        return this
+    }
+
+    fun append(value: T): LinkedList<T> {
+        if (isEmpty()) {
+            push(value)
+            return this
+        }
+        tail?.next = Node(value = value)
+        tail = tail?.next
+        size++
+        return this
+    }
+
+}
---
+++ app/src/main/java/com/example/linkedlist/SecondFragment.kt
@@ -0,0 +1,50 @@
+package com.example.linkedlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.linkedlist.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val list = LinkedList<Int>()
+
+        list.push(3).push(2).push(1)
+        list.append(4).append(5).append(6)
+
+        binding.apply {
+            textviewSecond.text = list.toString()
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
